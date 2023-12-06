d3a4ce43495fd1de0abadf2c4e1356567fc84e1f
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-12-06 19:32:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -437,6 +437,7 @@ elif st.session_state['page'] == 'Analyse':
             - Origine : Cellules de 106 patients de l’Hôpital Johns Hopkins, atteints de Leucémie Myéloïde Aiguë (AML) ou de Leucémie Aiguë Promyélocytaire (APL)
             
             Ce jeu de données contient des images de cellules de patients atteints de deux types de leucémie. Les cellules sont classées dans des dossiers par catégorie, et il existe également un dossier pour les cellules non classées.
+            )
             '''
         
             # Afficher df_data_APL
