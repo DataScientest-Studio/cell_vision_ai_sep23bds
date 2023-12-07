8984d919422c8c17b2fed40bacbcd391f43f7894
miklderoche <127089400+miklderoche@users.noreply.github.com>
2023-12-07 18:14:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -808,41 +808,32 @@ elif st.session_state['page'] == 'Machine learning':
         
         """)
 
-        text = """
-                         precision    recall  f1-score   support
-                         
-                   0       0.00      0.00      0.00        16
-                   1       0.49      0.67      0.57       246
-                   2       0.74      0.84      0.78       341
-                   3       0.79      0.86      0.82       623
-                   4       0.69      0.53      0.60       363
-                   5       0.00      0.00      0.00         6
-                   6       0.67      0.52      0.59       556
-                   7       0.68      0.87      0.76       655
-                   8       0.12      0.02      0.03        58
-                   9       0.62      0.56      0.59       408
-                  10       0.87      0.87      0.87       842
-                  11       0.00      0.00      0.00         4
-                  12       0.91      0.79      0.85       421
-                  13       0.00      0.00      0.00         4
+        # Les noms de colonnes explicites
+        columns_keras = ["Class", "Precision", "Recall", "F1-Score", "Support"]
         
-            accuracy                           0.73      4543
-           macro avg       0.47      0.47      0.46      4543
-        weighted avg       0.73      0.73      0.72      4543
-        """
-
-        # Séparer le texte en lignes
-        lines = text.split("\n")
+        # Les données
+        data_keras = [
+            [0, 0.00, 0.00, 0.00, 16],
+            [1, 0.49, 0.67, 0.57, 246],
+            [2, 0.74, 0.84, 0.78, 341],
+            [3, 0.79, 0.86, 0.82, 623],
+            [4, 0.69, 0.53, 0.60, 363],
+            [5, 0.00, 0.00, 0.00, 6],
+            [6, 0.67, 0.52, 0.59, 556],
+            [7, 0.68, 0.87, 0.76, 655],
+            [8, 0.12, 0.02, 0.03, 58],
+            [9, 0.62, 0.56, 0.59, 408],
+            [10, 0.87, 0.87, 0.87, 842],
+            [11, 0.00, 0.00, 0.00, 4],
+            [12, 0.91, 0.79, 0.85, 421],
+            [13, 0.00, 0.00, 0.00, 4]
+        ]
         
-        # Extraire la ligne des noms de colonnes
-        header = lines[1]
+        # Créer le DataFrame pandas
+        df_keras = pd.DataFrame(data_keras, columns=columns_keras)
         
-        # Afficher les noms de colonnes en haut de chaque colonne
-        for i in range(len(header)):
-            st.text(header[i])
-        
-        # Afficher le reste du texte sans la ligne des noms de colonnes
-        st.text('\n'.join(lines[2:]))
+        # Afficher le DataFrame dans Streamlit
+        st.write(df_keras)
 
         st.image('images/ML_Keras.jpg')
         
